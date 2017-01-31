009f7196d988024cd8160e228ae2669980d570d3
Saem <ottosigma@gmail.com>
2017-01-31 21:27:54 +0200

    webtarget with template resolver

--- src/main/java/com/sameperson/client/Main.java
+++ src/main/java/com/sameperson/client/Main.java
@@ -4,6 +4,8 @@ import com.sameperson.client.entity.Message;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class Main {
@@ -11,8 +13,15 @@ public class Main {
     public static void main(String[] args) {
 
         Client client = ClientBuilder.newClient();
-        Response response = client.target("http://localhost:8080/webapi/messages/1").request().get();
-        Message message = response.readEntity(Message.class);
+
+        WebTarget baseTarget = client.target("http://localhost:8080/webapi/");
+        WebTarget messagesTarget = baseTarget.path("messages");
+        WebTarget singleMessageTarget = messagesTarget.path("{messageId}");
+
+        Message message = singleMessageTarget
+                .resolveTemplate("messageId", 1)
+                .request(MediaType.APPLICATION_JSON)
+                .get(Message.class);
         System.out.println(message.getMessage());
 
     }
